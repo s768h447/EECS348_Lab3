b8e0cb9a26fc072f91c4ba291e2f5fe3bcd2032d
s768h447 <123592334+s768h447@users.noreply.github.com>
2023-02-11 20:14:18 -0600

    Add files via upload

--- lab3_1.c
+++ lab3_1.c
@@ -1,57 +1,212 @@
 #include <stdio.h>
+#include <stdlib.h>
+float* read_data_from(char *file_name) {
 
-int main() {
-    float monthly_sales[] = {
-    23458.01, 
-    40112.00, 
-    56011.85, 
-    37820.88, 
-    37904.67,
-    60200.22,
-    72400.31,
-    56210.89,
-    67230.84,
-    68233.12,
-    80950.34,
-    95225.22};
+    //  allocates data for array
+    float* month_sales_data = (float*)malloc(12 * sizeof(float));
 
+    //  attempts to open file
+    FILE *fp = fopen(file_name, "r+");
+
+    //  informs of potential failure
+    if(fp == NULL) {
+        printf("Invalid File\n");
+    }
+
+    
+    for(int i = 0; i < 12; ++i) {
+        fscanf(fp, "%f\n", &month_sales_data[i]);
+    }
+
+    return month_sales_data;
+
+}
+
+void salesReport(float data[11]) {
+
+    //  prints all of the following
     printf("Monthly sales report for 2022\n\n");
-    printf("January %.2f ", monthly_sales[0]);
+    printf("January %.2f ", data[0]);
     printf("\n");
-    printf("February %.2f ", monthly_sales[1]);
+    printf("February %.2f ", data[1]);
     printf("\n");
-    printf("March %.2f ", monthly_sales[2]);
+    printf("March %.2f ", data[2]);
     printf("\n");
-    printf("April %.2f ", monthly_sales[3]);
+    printf("April %.2f ", data[3]);
     printf("\n");
-    printf("May %.2f ", monthly_sales[4]);
+    printf("May %.2f ", data[4]);
     printf("\n");
-    printf("June %.2f ", monthly_sales[5]);
+    printf("June %.2f ", data[5]);
     printf("\n");
-    printf("July %.2f ", monthly_sales[6]);
+    printf("July %.2f ", data[6]);
     printf("\n");
-    printf("August %.2f ", monthly_sales[7]);
+    printf("August %.2f ", data[7]);
     printf("\n");
-    printf("September %.2f ", monthly_sales[8]);
+    printf("September %.2f ", data[8]);
     printf("\n");
-    printf("October %.2f ", monthly_sales[9]);
+    printf("October %.2f ", data[9]);
     printf("\n");
-    printf("November %.2f ", monthly_sales[10]);
+    printf("November %.2f ", data[10]);
     printf("\n");
-    printf("December %.2f ", monthly_sales[11]);
+    printf("December %.2f ", data[11]);
     printf("\n\n");
+    }
+
+void salesSummary(float data[11]) {
 
     printf("Sales summary:\n\n");
-    printf("Minimum sales: 23458.01 (January)\nMaximum sales: 95225.22 (December)\nAverage sales: 57979.86\n");
 
-    printf("\nSix-Month moving average report:\n\n");
-    printf("January-June\t42584.61\nFebruary-July\t50741.66\nMarch-August\t53424.80\nApril-September\t55294.64\nMay-October\t60363.34\nJune-November\t56537.62\nJuly-December\t73375.12\n");
+    char months[11][20] = {
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "November",
+        "December"
+    };
 
-    printf("\nSales report (highest to lowest):\n\n");
-    printf("Month\tSales\n");
-    printf("December\t95225.22\nNovember\t80950.34\nJuly\t72400.31\nOctober\t68233.12\nSeptember\t67230.84\nJune\t60200.22\nAugust\t56210.89\nMarch\t56011.85\nFebruary\t40112.00\nMay\t37904.67\nApril\t37820.88\nJanuary\t23458.01");
+    int i, num = 0;
+    float min = data[0];
+    float max = data[0];
+    float avg = 0;
+
+
+    //  minimum loop
+    for(i = 0; i < 11; i++) {
+        if(data[i] < min) {
+            min = data[i];
+            num = i;
+            }
+        }
+
+    //  minimum print
+    printf("Minimum sales:\t%.2f", min);
+    printf("  %s\n", months[num]);
+
+    //  maximum loop
+    for(i = 0; i < 11; i++) {
+        if(data[i] > max) {
+            max = data[i];
+            num = i;
+            }
+        }
+
+    //  maximum print
+    printf("Maximum sales:\t%.2f", max);
+    printf("  %s\n", months[num]);
+
+    for(i = 0; i < 11; i++) {
+        avg += data[i];
+    }
+    
+    avg = avg / 12;
+
+    printf("Average sales:\t%.2f", avg);
+
+    }
+
+void movingAverage(float data[11]) {
+    
+    printf("\n\nSix-Month Moving Average Report:\n\n");
+
+    char months[11][20] = {
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "November",
+        "December"
+    };
+
+    float jj = (data[0] + data[1] + data[2] + data[3] + data[4] + data[5])/6;
+    float fj = (data[1] + data[2] + data[3] + data[4] + data[5] + data[6])/6;
+    float ma = (data[7] + data[2] + data[3] + data[4] + data[5] + data[6])/6;
+    float as = (data[7] + data[8] + data[3] + data[4] + data[5] + data[6])/6;
+    float mo = (data[7] + data[8] + data[9] + data[4] + data[5] + data[6])/6;
+    float jn = (data[7] + data[8] + data[9] + data[10] + data[5] + data[6])/6;
+    float jd = (data[7] + data[8] + data[9] + data[10] + data[11] + data[6])/6;
+
+    printf("January - June\t%.2f\n", jj);
+    printf("February - July\t%.2f\n", fj);
+    printf("March - August\t%.2f\n", ma);
+    printf("April - September\t%.2f\n", as);
+    printf("May - October\t%.2f\n", mo);
+    printf("June - November\t%.2f\n", jn);
+    printf("July - December\t%.2f\n", jd);
+}
+
+void salesReportt(float data[11]) {
+
+    printf("\n\nSales Report (Highest to Lowest):\n\n");
+
+    char months[11][20] = {
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "November",
+        "December"
+    };   
+
+    float t = 0;
+
+    for (int i = 0; i < 11; i++) {
+        for (int j = i + 1; j < 11; j++) {
+            if(data[i] < data[j]) {
+                t = data[i];
+                data[i] = data[j];
+                data[j] = t;
+            }
+        }
+    }
+
+    for (int i = 0; i < 11; i++) {
+        printf("%.2f\n", data[i]);
+    }
+
+
+}
+
+int main() {
+
+    //  variable storing file to input
+    char file_name[25];
+
+    printf("Attempt 3\n");
+
+    //  gets file 
+    printf("Input File: ");
+    scanf("\n%s", file_name);
+
+    //  reads file
+    float* month_sales_data = read_data_from(file_name);
+
+    //  sales report
+    salesReport(month_sales_data);
+
+    //  sales summary
+    salesSummary(month_sales_data);
+
+    //  six-month 
+    movingAverage(month_sales_data);
+
+    salesReportt(month_sales_data);
 
     system("pause");
     return 0;
-
 }
--- lab3_2.c
+++ lab3_2.c
@@ -1,20 +1,46 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(){
+//  adds to score and then goes to next loop if <= data, repeats to bottom, and if is totaled, prints as a possible scoring
+void possibleCombinations(int data) {
+    printf("\npossible combinations of scoring plays:\n");
 
-    int score;
+	for (int takedown2 = 0; takedown2 <= data / 8; takedown2++) {
 
-    printf("Enter 0 or 1 to STOP\n");
-    printf("Input NFL Score: ");
-    scanf("%d", &score);
-    printf("\n");
+		for (int takedown1 = 0; takedown1 <= data / 7; takedown1++) {
 
-    printf("possible combination of scoring plays:\n");
+			for (int takedown = 0; takedown <= data / 6; takedown++) {
 
+				for (int field_goal = 0; field_goal <= data / 3; field_goal++) {
 
-    int endprogram;
+					for (int safety = 0; safety <= data / 2; safety++) {
 
-    scanf("%d", &endprogram);
+						if ((takedown2 * 8 + takedown1 * 7 + takedown * 6 + field_goal * 3 + safety * 2) == data) {
 
-    return endprogram;
+							printf("%d TD + 2pt, %d TD + FG, %d TD, %d 3pt FG, %d Safety\n", takedown2, takedown1, takedown, field_goal, safety);
+							}
+						}
+					}
+				}
+			}
+	}
+}
+
+int main() {
+	int score = 0;
+
+    //  prints before scanning
+	printf("Enter 0 or 1 to STOP\nEnter the NFL score: ");
+	scanf("%d", &score);
+
+    if (score == (0 || 1)) {
+        return 0;
+    }
+
+    //  runs function
+    possibleCombinations(score);
+
+    //  I don't like it when the program ends and don't know what else to use
+    system("pause");
+	return 0;
 }
